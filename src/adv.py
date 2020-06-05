9ad12cf74f6e3aead2963de50c98592dd58f1808
Carlos Alvarado <Carlosalvaradoarguello10@gmail.com>
2020-06-05 10:03:57 -0500

    almost done with game

--- src/adv.py
+++ src/adv.py
@@ -35,51 +35,97 @@ room['treasure'].s_to = room['narrow']
 
 #
 # Main
-#
+
+#tries to move the player in the specified direction 
 def try_direction(player, direction):
+    # check the player's current location and see if there is 
+    # a room in the specified direction
+    # if there is, move them there to that room
+    # otherwise, a print message saying "we can't go there" and 
+    # not move the player
 
     attribute = direction + '_to'
 
     # python has a handy method called hasattr
     # which allows us to check if a class has an attribute
     if hasattr(player.location, attribute):
-        # this is valid direction
-        # use getattr to fetch the value associated with 
-        player.location = getattr(player.location)
-    else:
-        print("There's nothing in that direction")
+        #this is a valid direction
+        # use getattr to fetch the value associated with the attribute
+        # update our player's location with the fetched room
+        player.location = getattr(player.location, attribute)
+    else: 
+        print('\n')
+        print("There is nothing in that direction!")
+
+
+
+
+
 # Make a new player object that is currently in the 'outside' room.
 
-player = Player(room['outside '])
+player = Player(room['outside'], "Jim")
+
+player.location.addItem({ 'name': "Aphrodites Bow", "desc":"A bow and arrow that can incinerate an opponent or grants you mind control over a player for a fixed time"})
+
+
 # Write a loop that:
 while True:
+    # * Prints the current description  and location(the textwrap module might be useful here). 
+    print('\n')
     print(player.location)
 
+    # print out list of items if they exist in a room
+
+    if len(player.location.items) > 0:
+        print('\n')
+        print("Available items in this room:")
+        print('\n')
+        for i in player.location.items:
+            
+            print(f"{i['name']}: {i['desc']}") # prints out the name and desc of item
+
+    else:
+        print('\n')
+        print("There are no items in this room!")
+
+    # * Waits for user input and decides what to do.
     command = input("\nCommand: ").strip().lower().split()
-    print(command)
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
+    print(command[0])
+    
+
+
+
+# if the first letter is get and the last word is item name
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
-if command == 'quit':
-    print('you have quit the game')
 
-first_comm = command[0]
-first_char = first_comm[0]
 
-if first_char == 'n':
-    #move to the north
-    try_direction(player, command)
+    
+    
 
-elif first_char == 's':
-    try_direction(player, command)
-    #move to the south 
-elif first_char == 'e':
-    try_direction(player, command)
-    #move to the east 
-elif first_char == 'w':
-    try_direction(player, command)
+    command2 = command[0]
+
+    first_word = command2[0]
+
+    if first_word == 'q':
+        break
+
+
+    # first_char = str(command[0])
+    # print(first_char,'the first char')
+
+    if first_word == 'n':
+        #move to the north
+        try_direction(player, first_word)
+
+    elif first_word == 's':
+        # move to the south
+        try_direction(player, first_word)
+    elif first_word == 'w':
+        #move to the west
+        try_direction(player, first_word)
+    elif first_word == 'e':
+        #move to the east
+        try_direction(player, first_word)
--- src/room.py
+++ src/room.py
@@ -2,6 +2,31 @@
 # description attributes.
 
 class Room():
-    def __init__(self, name, location):
+    def __init__(self, name, description):
         self.name = name
-        self.location = location
+        self.description = description
+        self.items = []
+
+    def addItem(self, array):
+        self.items.append(array)
+        for i in self.items:
+            print(f"the items in the list {i}")
+
+    def __str__(self):
+        return f"{self.name}: {self.description}"
+
+# newRoom = Room("chungus", "The land of all chungs")
+
+# newRoom.addItem({"item":"Heavenly Sword"})
+
+# if len(newRoom.items) > 0:
+#     for i in newRoom.items:
+#         print(f"Each Item itterated: {i}")
+
+# else:
+#     print("There are no items in this room!")
+
+# print(newRoom,'The new room with items')
+
+# print(newRoom.addItem({"item":"Heavenly Sword"}), 'the item printed')
+
